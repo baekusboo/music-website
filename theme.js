296e0d60fc07e0e64a83842472f4d362ce7f2c97
nisoojadhav <nisoojadhav@gmail.com>
2023-10-22 14:00:35 +0530

    localstorage based theme preference

--- theme.js
+++ theme.js
@@ -1,17 +1,30 @@
-const themeIcon = document.querySelector('.toggle-label i');
+const themeIcon = document.querySelector(".toggle-label i");
 const body = document.body;
+const arrow = document.querySelector("#backToTopBtn");
+let musicitem = document.getElementsByClassName("music-item");
+let isDarkMode = localStorage.getItem("isDarkMode") === "true";
 
-let musicitem = document.getElementsByClassName('music-item');
-// console.log(musicitem);
-let isDarkMode = true;
+body.classList.toggle("dark-mode", isDarkMode);
 
-themeIcon.addEventListener('click', () => {
-    isDarkMode = !isDarkMode;
-    body.classList.toggle('dark-mode', isDarkMode);
+for (let i = 0; i < musicitem.length; i++) {
+  musicitem[i].classList.toggle("music-item-dark-mode", isDarkMode);
+}
 
-    for (let i = 0; i < musicitem.length; i++) {
-        musicitem[i].classList.toggle('music-item-dark-mode', isDarkMode);
-    }
-    themeIcon.classList.toggle('fa-sun', isDarkMode);
-    themeIcon.classList.toggle('fa-moon', !isDarkMode);
+themeIcon.classList.toggle("fa-sun", isDarkMode);
+themeIcon.classList.toggle("fa-moon", !isDarkMode);
+arrow.classList.toggle("light-mode", !isDarkMode);
+arrow.classList.toggle("dark-mode", isDarkMode);
+
+themeIcon.addEventListener("click", () => {
+  isDarkMode = !isDarkMode;
+  body.classList.toggle("dark-mode", isDarkMode);
+
+  for (let i = 0; i < musicitem.length; i++) {
+    musicitem[i].classList.toggle("music-item-dark-mode", isDarkMode);
+  }
+  themeIcon.classList.toggle("fa-sun", isDarkMode);
+  themeIcon.classList.toggle("fa-moon", !isDarkMode);
+  arrow.classList.toggle("light-mode", !isDarkMode);
+  arrow.classList.toggle("dark-mode", isDarkMode);
+  localStorage.setItem("isDarkMode", isDarkMode);
 });
